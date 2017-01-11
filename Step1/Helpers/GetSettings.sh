c57329d26a8063e5ec416deba91f7c2a8eaf16ab
a-d-smith <andrewsmithweb@gmail.com>
2017-01-11 16:26:19 +0000

    Started to add helper functions for step 1

---
+++ Step1/Helpers/GetSettings.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $# -ne 4 ]; then
+    echo "Please pass the following arguments"
+    echo "  1) pre / post"
+    echo "  2) input file"
+    echo "  3) file id "
+    echo "  4) output file"
+fi
